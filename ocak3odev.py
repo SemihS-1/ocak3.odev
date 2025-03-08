502ae76779a27f69f0a9521f4edd418eb2d3d582
Semih Ada Şahin <semihadashn@gmail.com>
2025-03-08 17:30:25 +0300

    Add files via upload

---
+++ ocak3odev.py
@@ -0,0 +1,11 @@
+sayi = int(input("sayi giriniz:"))
+
+for a in range(0, sayi+1):
+    print(a)
+
+sayi = int(input("sayi giriniz:"))
+toplam = 0
+
+while toplam in range(0, sayi):
+    toplam = toplam + 1
+    print(toplam)
